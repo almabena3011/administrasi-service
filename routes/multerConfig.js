8a119b8056bea9bab7caa5b5ee4543417d7423b9
Albet Matthew Best Nainggolan <golanbrother@gmail.com>
2023-06-04 22:26:08 +0700

    update multer config for validation

--- routes/multerConfig.js
+++ routes/multerConfig.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, './proposal')); // Ubah ini sesuai struktur direktori Anda
+        cb(null, path.join(__dirname, '../public/proposals')); // Ubah ini sesuai struktur direktori Anda
     },
     filename: function (req, file, cb) {
         const ext = path.extname(file.originalname);
@@ -13,14 +13,39 @@ const storage = multer.diskStorage({
     },
 });
 
-const upload = multer({ storage: storage }).single('dokumen_proposal');
+const fileFilter = function (req, file, cb) {
+    // Accept PDFs only
+    if (!file.originalname.match(/\.(pdf)$/)) {
+        req.fileValidationError = 'Hanya file PDF yang diperbolehkan!';
+        return cb(new Error('Hanya file PDF yang diperbolehkan!'), false);
+    }
+    cb(null, true);
+};
+
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 2 * 1024 * 1024 // Limit file size to 2MB
+    }
+}).single('dokumen_proposal');
 
 const uploadMiddleware = (req, res, next) => {
     upload(req, res, function (err) {
         if (err instanceof multer.MulterError) {
+            if (err.code === 'LIMIT_FILE_SIZE') {
+                err.message = 'Ukuran file terlalu besar! Maksimum adalah 2MB';
+                console.log(err.message);
+                return res.status(400).json(err.message);
+            }
             console.log('error: ', err.message);
             return res.status(500).json(err.message);
         } else if (err) {
+            if (req.fileValidationError) {
+                console.log(req.fileValidationError);
+                return res.status(400).json(req.fileValidationError);
+            }
             console.log(err.message);
             return res.status(500).json(err.message);
         }
