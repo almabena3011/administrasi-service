1526127b80120c54f32f7436fe733b7275ca6ea2
Albet Matthew Best Nainggolan <golanbrother@gmail.com>
2023-06-11 17:26:53 +0700

    update

--- routes/handler/proposal/getAllProposalByMahasiswaId.js
+++ routes/handler/proposal/getAllProposalByMahasiswaId.js
@@ -1,18 +1,28 @@
 const { Proposal } = require('../../../models');
+const { getMahasiswaById } = require('../userService');
 
 module.exports = async (req, res) => {
     try {
         const mahasiswaId = req.params.mahasiswaId;
+        const mahasiswa = await getMahasiswaById(mahasiswaId);
         const proposals = await Proposal.findAll({
             where: {
                 mahasiswaId: mahasiswaId,
             }
         });
-        res.status(200).json({
+        return res.status(200).json({
             status: 'success',
             data: proposals
         });
     } catch (error) {
-        res.status(500).json({ error: 'An error occurred' });
+        if (error.message === 'Mahasiswa not found') {
+            res.status(404).json({
+                error: 'Mahasiswa not found'
+            });
+        } else if (error.message === 'User service is not available') {
+            res.status(500).json({ error: 'User service is not available' });
+        } else {
+            res.status(500).json({ error: 'ERROR OI' });
+        }
     }
 }
--- routes/handler/userService.js
+++ routes/handler/userService.js
@@ -38,7 +38,25 @@ async function getDosenById(id) {
     }
 }
 
+async function getMahasiswaById(id) {
+    try {
+        const response = await axios.get(`${URL_SERVICE_USER}/mahasiswa/${id}`);
+        if (!response.data) {
+            throw new Error('Mahasiswa not found');
+        }
+        return response.data.data;
+    } catch (error) {
+        if (error.response && error.response.data && error.response.data.error) {
+            throw new Error(error.response.data.error);
+        } else if (error.response && error.response.status === 404) {
+            throw new Error('Mahasiswa not found');
+        } else {
+            throw new Error('User service is not available');
+        }
+    }
+}
 
 
-module.exports = { getMahasiswaByAuthId, getDosenById };
+
+module.exports = { getMahasiswaByAuthId, getDosenById, getMahasiswaById };
 
