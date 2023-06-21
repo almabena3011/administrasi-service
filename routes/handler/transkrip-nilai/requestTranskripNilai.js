f5630a0955c2c5332436ab3e064b701022533a16
Timothy Sipahutar <ifs19016@students.del.ac.id>
2023-06-21 17:39:13 +0700

    minor update

--- routes/handler/transkrip-nilai/requestTranskripNilai.js
+++ routes/handler/transkrip-nilai/requestTranskripNilai.js
@@ -1,25 +1,31 @@
-const { Proposal } = require('../../../models');
+const { Proposal } = require("../../../models");
 
 module.exports = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const proposal = await Proposal.findOne({
+      where: {
+        id: id,
+      },
+    });
 
-    const id = req.params.id
-    try {
-        const proposal = await Proposal.findOne({
-            where: {
-                id: id
-            }
-        });
-
-        console.log(proposal);
-        proposal.transkrip_path = req.file.path;
-        proposal.is_transkrip_generated = true;
-
-        await proposal.save();
-        return res.status(200).json({
-            status: 'success',
-            message: 'Transkrip nilai berhasil diterbitkan'
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    if (!proposal) {
+      return res.status(404).json({
+        status: "error",
+        message: "Proposal not found",
+      });
     }
-}
+
+    console.log(proposal);
+    proposal.transkrip_path = req.file.path;
+    proposal.is_transkrip_generated = true;
+
+    await proposal.save();
+    return res.status(200).json({
+      status: "success",
+      message: "Transkrip nilai berhasil diterbitkan",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
